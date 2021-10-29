9e618af329149880c62e2fe87706003a895db524
Paula Alvarez <paulaalpe@gmail.com>
2021-10-29 19:36:48 +0200

    Iteration#2

--- index.js
+++ index.js
@@ -7,4 +7,25 @@ function sum(numberOne , numberTwo) {
     }
 }
 
+/*Iteración #2: Buscar la palabra más larga**
+Completa la función que tomando un array de strings como argumento devuelva el más largo, en caso de que dos strings tenga la misma longitud deberá devolver el primero.
+*/
+const avengers = ['Hulk', 'Thor', 'IronMan', 'Captain A.', 'Spiderman', 'Captain M.'];
+
+function findLongestWord(param) {
+
+    let longitudMaximaElemento = 0;
+    let elementoMasLargo = "";
+
+    for(let i =0; i<param.length; i++){
+      let longitudElemento = param[i].length;
+      if (longitudElemento > longitudMaximaElemento){
+        longitudMaximaElemento = longitudElemento;
+        elementoMasLargo = param[i];
+      } 
+    };
+    return elementoMasLargo;
+}
+
+
 
