29b063f285110aca0402a6ad30b2f6bca2a45a4d
Paula Alvarez <paulaalpe@gmail.com>
2021-10-29 18:50:59 +0200

    Iteration#1

---
+++ index.js
@@ -0,0 +1,11 @@
+//OK Iteración#1: Buscar el máximo. Completa la función que tomando dos números como argumento devuelva el más alto.
+function sum(numberOne , numberTwo) {
+    if (numberOne > numberTwo) {
+        return numberOne;
+    } else {
+        if (numberTwo > numberOne)
+        return numberTwo
+}
+}
+
+
