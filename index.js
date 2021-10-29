c0fa8620ac04a19bcb2c3d2ea0c489eb3268079b
Paula Alvarez <paulaalpe@gmail.com>
2021-10-29 18:59:33 +0200

    Iteration#1_fix

--- index.js
+++ index.js
@@ -3,9 +3,8 @@ function sum(numberOne , numberTwo) {
     if (numberOne > numberTwo) {
         return numberOne;
     } else {
-        if (numberTwo > numberOne)
-        return numberTwo
-}
+        return numberTwo;
+    }
 }
 
 
