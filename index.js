969defa3f391a33c75d5ec1771f7137163209a8a
Paulaalpe <91777530+Paulaalpe@users.noreply.github.com>
2021-11-16 22:09:11 +0100

    Iteration#8

--- index.js
+++ index.js
@@ -121,3 +121,31 @@ function finderName(param,valor) {
 }
 // el return podría ser un string concatenando "true/false" con i
 
+// Iteration #8: Contador de repeticiones
+//Crea una función que nos devuelva el número de veces que se repite cada una de las palabras que lo conforma. 
+const counterWords = [
+  'code',
+  'repeat',
+  'eat',
+  'sleep',
+  'code',
+  'enjoy',
+  'sleep',
+  'code',
+  'enjoy',
+  'upgrade',
+  'code'
+];
+function repeatCounter(param) {
+  let temporal = {};
+  for(let i = 0; i <param.length; i++) {
+    const elemento = param[i];
+    if(temporal[elemento] !== undefined) {
+      temporal[elemento] ++;
+    }  else {
+         temporal[elemento] = 1;
+    }
+  }
+  console.log(temporal);
+} 
+repeatCounter(counterWords);
